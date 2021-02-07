510e357b3c5942379f6ac398f998fa8ca48afd04
andrew stehno <andrew_stehno@yahoo.com>
2021-02-06 17:02:49 -0700

    styling Our Features section to reveal upon scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,12 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
 if(module.hot){
     module.hot.accept()
 }
+
+let revealOnScroll = new RevealOnScroll();
 
 let mobileMenu = new MobileMenu();
 
