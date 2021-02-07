fe63c2c43803c6a9cdc2b4475188bf2c07f420ff
andrew stehno <andrew_stehno@yahoo.com>
2021-02-06 21:16:51 -0700

    recycled reveal on scroll code to work on multiple sections

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,7 +6,8 @@ if(module.hot){
     module.hot.accept()
 }
 
-let revealOnScroll = new RevealOnScroll();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
 let mobileMenu = new MobileMenu();
 
